f116422166289bbc8d4b98795f7d079d59028821
yashbonde <bonde.yash97@gmail.com>
2020-12-17 12:21:12 +0530

    add gemm test sample

--- libtorch_example/attn.cpp
+++ libtorch_example/attn.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <torch/torch.h>
+#include <ATen/ATen.h>
 
 torch::Tensor dot_prod(torch::Tensor t1, torch::Tensor t2) {
     std::cout << "Inputs\n" << t1 << "\n" << t2 << std::endl;
@@ -16,9 +17,24 @@ torch::Tensor dot_prod(torch::Tensor t1, torch::Tensor t2) {
 }
 
 // main function
-int main (){
-    torch::Tensor t1 = torch::rand({2, 3});
-    torch::Tensor t2 = -torch::rand({2, 3});
-    torch::Tensor out = dot_prod(t1, t2);
-    std::cout << "output:\n" << out << std::endl;
+int main (){ 
+    // torch::Tensor t1 = torch::rand({2, 3});
+    // torch::Tensor t2 = -torch::rand({2, 3});
+    // torch::Tensor out = dot_prod(t1, t2);
+    // std::cout << "output:\n" << out << std::endl;
+
+    int B = 1;
+    int N = 2;
+    int H = 2;
+    int E = 4;
+
+    torch::Tensor t1 = torch::rand({B, H, N, E / H});
+    torch::Tensor t2 = torch::rand({B, H, N, E / H});
+
+    // torch::Tensor t1 = torch::rand({1, 2});
+    // torch::Tensor t2 = torch::rand({2, 3});
+    std::cout << "Inputs\n" << t1 << "\n" << t2 << std::endl;
+    
+    torch::Tensor out = at::matmul(t1, t2);
+    std::cout << out;
 }
