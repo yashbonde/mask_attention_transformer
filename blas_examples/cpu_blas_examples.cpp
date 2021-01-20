126d08674d09e101415caa974dc806ea92a2df2a
yashbonde <bonde.yash97@gmail.com>
2021-01-20 11:12:05 +0530

    test blas example

--- blas_examples/cpu_blas_examples.cpp
+++ blas_examples/cpu_blas_examples.cpp
@@ -1,62 +1,79 @@
 // In this exmaple I am trying to implement the BLAS versions for
 // GEMM ops.
 // dgemm: https://www.netlib.org/lapack/explore-html/d1/d54/group__double__blas__level3_gaeda3cbd99c8fb834a60a6412878226e1.html
-#include <stdio.h>
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <ATen/native/CPUBlas.h>
 
+// my implementation of: https://github.com/flame/how-to-optimize-gemm/blob/master/src/MMult_4x4_14.c
+#define min( i, j ) ( (i)<(j) ? (i): (j) )
+#define mc 256
+#define kc 128
+
+void random_matrix( int m, int n, double *a) {
+  for (int i=0; i<m; i++ ){
+    for (int j=0; j<n; j++ ){
+      // note in memory we are storing all these values as a contiguous array
+      // so we need to get the proper indices
+      a[i*n +j] = 2.0 * drand48() - 1.0;
+  }}
+}
+
+void print_matrix(int m, int n, double *a) {
+  for (int i=0; i<m; i++ ){
+    for (int j=0; j<n; j++ ){
+      std::cout << a[i*n + j] << " ";}
+    std::cout << "\n";}
+}
+
+void print_number_of_values(int m, int n, double *a) {
+  int cntr = 0;
+  for (int i=0; i<m; i++ ){
+    for (int j=0; j<n; j++ ){
+      cntr += 1;
+  }}
+  std::cout << "number of tokens: " << cntr;
+}
+
+void my_gemm(
+  // char transa, char transb,
+  int m, int n, int k,
+  // int alpha = 1, int beta = 0,
+  double *a, int lda,
+  double *b, int ldb,
+  double *c, int ldc
+  ) {
+  // the arguments are same as for the GEMM operations
+
+}
+
 int main(){
-  double *a, *x, *y;
-  double alpha, beta;
-  int m, n, lda, incx, incy, i;
+  // In this demo we are calculating C = A x B
+  int m, n, k;
+  m = 40;
+  n = 50;
+  k = 60;
 
-  m = 4;   // Size of column (number of rows)
-  n = 4;   // Size of row (number of columns)
-  lda = 4; // Leading dimension; eg. for a 5x4 matrix is 5
-  incx = 1;
-  incy = 1;
-  alpha = 1;
-  beta = 0;
+  double *a = (double *) malloc (sizeof(double) * m * k); // A has shape m x k
+  double *b = (double *) malloc (sizeof(double) * k * n); // B has shape k x n
+  random_matrix(m, k, a);
+  random_matrix(k, n, b);
 
-  // now we define the matrices
-  a = (double *) malloc (sizeof(double) * m * n);
-  x = (double *) malloc (sizeof(double) * n);
-  y = (double *) malloc (sizeof(double) * n);
+  std::cout << "---- Matrix A ";
+  print_number_of_values(m,k,a);
+  std::cout << "\n---- Matrix B ";
+  print_number_of_values(k, n, b);
 
-  /* The elements of the first column */
-  a[0] = 1;
-  a[1] = 2;
-  a[2] = 3;
-  a[3] = 4;
-  /* The elements of the second column */
-  a[m] = 1;
-  a[m+1] = 1;
-  a[m+2] = 1;
-  a[m+3] = 1;
-  /* The elements of the third column */
-  a[m*2] = 3;
-  a[m*2+1] = 4;
-  a[m*2+2] = 5;
-  a[m*2+3] = 6;
-  /* The elements of the fourth column */
-  a[m*3] = 5;
-  a[m*3+1] = 6;
-  a[m*3+2] = 7;
-  a[m*3+3] = 8;
-  /* The elements of x and y */
-  x[0] = 1;
-  x[1] = 2;
-  x[2] = 1;
-  x[3] = 1;
-  y[0] = 0;
-  y[1] = 0;
-  y[2] = 0;
-  y[3] = 0;
+  // print if needed
+  if (false){
+    std::cout << "---- Matrix A\n";
+    print_matrix(m, k, a);
+    std::cout << "---- Matrix B\n";
+    print_matrix(k, n, b);
+    std::cout << "---------------------\n";
+  }
 
-  // this gives 0x7fe7f5d7b830 as this is a pointer we get the address
-  std::cout << a << "\n";
-  // use *a to get value for first and *a+1 to get value for second
-  std::cout << *a << " " << *a+1 << "\n";
-
-  
+  free (a);
+  free (b);
 }
